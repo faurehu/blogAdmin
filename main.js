4c75ff62409d5b9f8cb8a31bd38853dda90cf5c4
Faure Hu Chavez <faurehu@gmail.com>
2015-08-30 01:58:09 +0100

    Main.js renders index.html

---
+++ main.js
@@ -0,0 +1,26 @@
+var app = require('app')
+var BrowserWindow = require('browser-window');
+
+// Report crashes to our server.
+require('crash-reporter').start();
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadUrl('file://' + __dirname + '/views/index.html');
+  // mainWindow.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
